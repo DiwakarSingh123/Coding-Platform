ac63533328c716c27741b121e487dc0b01cff00f
Diwakar Singh <diwakarsinghdd111@gmail.com>
2025-08-17 17:45:47 +0530

    Removed node_modules and .env from repo, added to .gitignore

--- day_7/src/routes/submitProblem.js
+++ day_7/src/routes/submitProblem.js
@@ -7,14 +7,3 @@ submitRouter.post('/submit/:id',userMiddleware,submitCode);
 
 
 
-// echo "# Coding-Platform" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/DiwakarSingh123/Coding-Platform.git
-// git push -u origin main
-// …or push an existing repository from the command line
-// git remote add origin https://github.com/DiwakarSingh123/Coding-Platform.git
-// git branch -M main
-// git push -u origin main
